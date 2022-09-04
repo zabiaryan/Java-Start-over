da5fbcf3bf4eaf532fb10e38991b123f010c8f43
Aryan Zabi <83302669+zabiaryan@users.noreply.github.com>
2022-09-03 20:32:26 -0700

    Add files via upload

---
+++ DrawBoxes.java
@@ -0,0 +1,37 @@
+package Defaultt;
+
+public class Solution {
+
+	public static void main(String[] args) {
+			//Drawing two boxes in main function
+        /*System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println();
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");*/
+		drawBox();
+		System.out.println();
+		drawBox2();
+		
+		/***************************/
+		/*Using methods drawing boxes*/
+	}
+	public static void drawBox() {
+
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+	}
+	public static void drawBox2() {
+		    System.out.println("+------+");
+	        System.out.println("|      |");
+	        System.out.println("|      |");
+	        System.out.println("+------+");
+	}
+
+}
